ab0482110234360c7987f9d71cdc1f71be25af94
mstsurnalyakter <surnalyms@gmail.com>
2024-07-12 16:25:31 +0600

    install shadcn

---
+++ src/app/myname/page.tsx
@@ -0,0 +1,12 @@
+
+const myName = () => {
+  return (
+    <main className="h-full items-center flex justify-center flex-col">
+
+        <div>myName</div>
+        <button className="px-4 py-2 bg-blue-500 rounded my-3 hover:bg-blue-700">Test Button</button>
+    </main>
+  )
+}
+
+export default myName
